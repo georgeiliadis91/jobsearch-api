f2588527da8ad4944b4963d484d2b21ffdb77f8d
georgeiliadis91 <george.iliadis.91@gmail.com>
2020-01-22 23:30:23 +0200

    axios fetch call and DB update working, needs some final touches

--- cron/cronjob.js
+++ cron/cronjob.js
@@ -1,5 +1,51 @@
 const CronJob = require('cron').CronJob;
+const axios = require('axios');
+const Job = require('../models/job');
+require('dotenv').config();
+
+const baseUrl = process.env.FETCH_GIT_BASEURL;
 
 module.exports = new CronJob('*/20 * * * * *', () => {
-	console.log('Cron job run 20 seconds ');
+	fetchGithub();
 });
+
+const fetchGithub = () => {
+	let resultCount = 1,
+		onPage = 0;
+	const allJobs = [];
+
+	//fetch pages
+	while (onPage != 1) {
+		// const res = await fetch(`${baseUrl}?page=${onPage}`);
+		// const jobs = await res.json();
+		// // console.log({ jobs });
+		// console.log(jobs.length);
+		// resultCount = jobs.length;
+		// if (resultCount > 0) {
+		// 	allJobs.push(...jobs);
+		// 	onPage++;
+		// }
+
+		axios
+			.get(`${baseUrl}?page=${onPage}`)
+			.then(res =>
+				res.data.map(listing => {
+					Job.create({
+						id: listing.id,
+						type: listing.type,
+						url: listing.url,
+						created_at: listing.created_at,
+						company_url: listing.company_url,
+						location: listing.location,
+						title: listing.title,
+						description: listing.description,
+						how_to_apply: listing.how_to_apply,
+						company_logo: listing.company_logo
+					});
+				})
+			)
+			.catch(error => console.log(error));
+
+		onPage++;
+	}
+};
