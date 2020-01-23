5b80147fdd6a8afc22cc797f9d00cc4b8037143b
georgeiliadis91 <george.iliadis.91@gmail.com>
2020-01-23 17:13:25 +0200

    fixed cron job, replacement logic is only missing.

--- cron/cronjob.js
+++ cron/cronjob.js
@@ -5,47 +5,42 @@ require('dotenv').config();
 
 const baseUrl = process.env.FETCH_GIT_BASEURL;
 
-module.exports = new CronJob('*/20 * * * * *', () => {
+module.exports = new CronJob('* * */1 * * *', () => {
 	fetchGithub();
 });
 
-const fetchGithub = () => {
+async function fetchGithub() {
 	let resultCount = 1,
 		onPage = 0;
 	const allJobs = [];
 
 	//fetch pages
-	while (onPage != 1) {
-		// const res = await fetch(`${baseUrl}?page=${onPage}`);
-		// const jobs = await res.json();
-		// // console.log({ jobs });
-		// console.log(jobs.length);
-		// resultCount = jobs.length;
-		// if (resultCount > 0) {
-		// 	allJobs.push(...jobs);
-		// 	onPage++;
-		// }
-
-		axios
+	while (resultCount > 0) {
+		await axios
 			.get(`${baseUrl}?page=${onPage}`)
-			.then(res =>
-				res.data.map(listing => {
-					Job.create({
-						id: listing.id,
-						type: listing.type,
-						url: listing.url,
-						created_at: listing.created_at,
-						company_url: listing.company_url,
-						location: listing.location,
-						title: listing.title,
-						description: listing.description,
-						how_to_apply: listing.how_to_apply,
-						company_logo: listing.company_logo
+			.then(res => {
+				resultCount = res.data.length;
+				if (resultCount > 0) {
+					res.data.map(async listing => {
+						await Job.create({
+							id: listing.id,
+							type: listing.type,
+							url: listing.url,
+							created_at: listing.created_at,
+							company_url: listing.company_url,
+							location: listing.location,
+							title: listing.title,
+							description: listing.description,
+							how_to_apply: listing.how_to_apply,
+							company_logo: listing.company_logo
+						});
 					});
-				})
-			)
+				}
+			})
 			.catch(error => console.log(error));
 
 		onPage++;
+		console.log('resCOunt: ' + resultCount);
+		console.log('onPage: ' + onPage);
 	}
-};
+}
